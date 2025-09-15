6c9ff66abf9f63a7287270e069b9879026702026
md-sanim-mia <your-vercel-email@example.com>
2025-09-15 23:28:04 +0600

    check deploy

--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -3,3 +3,4 @@ import { PrismaClient } from "../../../generated/prisma"
 
 const prisma=new PrismaClient()
 export default prisma
+//export const prismaAdmin=new PrismaClient({datasources:{db:{url:process.env.DATABASE_URL_ADMIN}}})
